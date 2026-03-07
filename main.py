c2123494daf8ced84a20da2ba393f577c1e1b0ce
FidellaRafidaArianii <fidellarafidaariani@gmail.com>
2026-03-07 07:30:52 +0700

    Update main

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from gui.main_window import MainWindow
+from ui.main_window import MainWindow
 
+app = QApplication(sys.argv)
 
-def main():
-    app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
 
-    window = MainWindow()
-    window.show()
-
-    sys.exit(app.exec_())
-
-
-if __name__ == "__main__":
-    main()
+sys.exit(app.exec_())
